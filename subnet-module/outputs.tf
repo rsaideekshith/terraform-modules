254fa0930a8280f498a40a506b283985fa8bde4b
rsaideekshith <87260104+rsaideekshith@users.noreply.github.com>
2022-02-16 10:16:45 +0530

    Delete outputs.tf

--- subnet-module/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "mi_pubilc_subnet" {
-  value = aws_subnet.mi_public_subnet.id
-}
-output "mi_public_subnet_arn" {
-  value = aws_subnet.mi_public_subnet.arn
-}
-output "cidr_block" {
-  value = aws_subnet.cidr_block.arn
-}
