3e20dee69a9be716fc90d16603f2c1c5407e1310
rsaideekshith <87260104+rsaideekshith@users.noreply.github.com>
2022-02-16 10:15:55 +0530

    Delete vars.tf

--- ec2-module/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_count" {
-  type        = number
-  default     = "3"
-}
-variable "instance_type" {
-  description = "The type of instance to start"
-  type        = string
-  default     = "t2.micro"
-}
