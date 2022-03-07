33484d82101103e669fc7bcc119b87cf5fd1f27c
Sai Deekshith <rsaideekshith@gmail.com>
2022-03-07 18:40:34 +0530

    file

--- subnet-module/main.tf
+++ subnet-module/main.tf
@@ -1,8 +1,8 @@
-resource "aws_subnet" "my_subnet" {
+resource "aws_public_subnet" "my_subnet" {
   # value of the VPC Id that's created earlier (vpc module output)
   vpc_id = var.vpc_id
 
-  cidr_block = var.subnet_cidr_block
+  cidr_block = var.public_subnet_cidr_block
 
   tags = {
     Name = join("_", var.project, "PUB_SUBNET")
