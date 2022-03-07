67b2f92bb2852189a992a10e21d002d8bedf4df3
Sai Deekshith <rsaideekshith@gmail.com>
2022-03-07 22:02:25 +0530

    changes finished

--- vpc-modules/main.tf
+++ vpc-modules/main.tf
@@ -2,10 +2,11 @@ resource "aws_vpc" "myvpc" {
   cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name = join("_", var.project , "VPC")
+    Name = join("_", [var.project] , "VPC")
     Project = var.project
   }
 }
+
 
 
 
