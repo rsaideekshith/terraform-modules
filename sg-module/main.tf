7b230983f9c1b11904f49a5ab315556bb3d6442d
Sai Deekshith <rsaideekshith@gmail.com>
2022-03-08 18:23:49 +0530

    changes finished

--- sg-module/main.tf
+++ sg-module/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [var.public_subnet_cidr_block]
+    cidr_blocks      = ["10.20.0.0/28"]
   }
 
   # Outgoing traffic to public
--- vpc-modules/main.tf
+++ vpc-modules/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "myvpc" {
   cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name = join("_", [var.project] , "VPC")
+    Name = join("_", [var.project] , "VPC", list)
     Project = var.project
   }
 }
